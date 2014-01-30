11fe9db427e52261f0754737defda11d2693747d
feffi <feffi1@gmail.com>
2014-01-30 21:40:16 +0100

    added second user for testing

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 use Trails\Models\User;
 
 class UserTableSeeder extends Seeder {
@@ -7,11 +6,18 @@ class UserTableSeeder extends Seeder {
 	public function run() {
 		DB::table ('users')->delete ();
 		User::create (array (
-				'firstname' => 'Kevin',
-				'lastname' => 'Wennemuth',
-				'username' => 'feffi',
-				'email' => 'feffi@feffi.org',
-				'password' => Hash::make ('test')
+			'firstname' => 'Kevin',
+			'lastname' => 'Wennemuth',
+			'username' => 'feffi',
+			'email' => 'feffi@feffi.org',
+			'password' => Hash::make ('test')
+		));
+		User::create (array (
+			'firstname' => 'John',
+			'lastname' => 'Tester',
+			'username' => 'john',
+			'email' => 'john@feffi.org',
+			'password' => Hash::make ('john')
 		));
 	}
 }
