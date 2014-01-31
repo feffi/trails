7bb7827c5d526ef97f1a670f08f308944764380a
feffi <feffi1@gmail.com>
2014-01-31 14:31:48 +0100

    added enum to represent a tracks status

---
+++ app/lib/TrackStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Enum to represent a tracks status.
+ *
+ * @author feffi
+ *
+ */
+abstract class TrackStatus {
+	const PENDING = 'PENDING';
+	const PLANNED = 'PLANNED';
+	const HELD = 'HELD';
+}
